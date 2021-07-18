650ba075e7e451b8631ec097c2b4824dc8eb43ff
Jadson Ribeiro (Notebook) <jdsntj@gmail.com>
2021-07-18 14:08:55 -0300

    test: SubscribeButton

--- src/components/SubscribeButton/SubscribeButton.spec.tsx
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -10,7 +10,6 @@ jest.mock('next/router');
 describe('SubscribeButton component', () => {
   it('renders correctly', () => {
     const mockedUseSession = mocked(useSession);
-
     mockedUseSession.mockReturnValueOnce([null, false]);
 
     render(<SubscribeButton />);
@@ -18,10 +17,10 @@ describe('SubscribeButton component', () => {
     expect(screen.getByText('Subscribe now')).toBeInTheDocument();
   });
 
-  it('redirects user to sign in when not authenticated', () => {
+  it('redirects user to sign in when not authenticated and click to subscribe', () => {
     const signInMocked = mocked(signIn);
+
     const mockedUseSession = mocked(useSession);
-    
     mockedUseSession.mockReturnValueOnce([null, false]);
 
     render(<SubscribeButton />);
@@ -36,7 +35,6 @@ describe('SubscribeButton component', () => {
   it('redirects user to posts when authenticated', () => {
     const mockedUseRouter = mocked(useRouter);
     const mockedPush = jest.fn();
-
     mockedUseRouter.mockReturnValueOnce({
       push: mockedPush
     } as any);
